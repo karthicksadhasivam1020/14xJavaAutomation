91eb64323b84600b9ed75af02127a2787039d179
Karthick <karthicksadhasivam1020@gmail.com>
2026-03-01 10:04:13 +0530

    3rd commit

---
+++ src/ex_02_Java_Basics/Lab004_Keywords_Identifier.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basics;
+
+public class Lab004_Keywords_Identifier {
+    static void main(String[] args) {
+        System.out.println("Keywords and Identifiers");
+        System.out.println(2+2);
+        System.out.println(2*8);
+        System.out.println(3/2); //println always returns an integer
+        //difference between print and println
+        System.out.println("HI");
+        System.out.print("Good");
+        System.out.print("Bad");
+    }
+}
