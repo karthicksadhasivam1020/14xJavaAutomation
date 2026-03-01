e6c7e0119a466aec2e8d2538ef8341e7efde9cd6
Karthick <karthicksadhasivam1020@gmail.com>
2026-03-01 09:38:32 +0530

    Second commint

---
+++ src/ex_02_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab002_Comments {
+//    This is line to add some comment for the information
+    public static void main(String[] args){
+        /*
+        System.out.println("Good Morning");
+        System.out.println("Good Evening");
+        */
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab003_Comments.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab003_Comments {
+/*
+This is the comments for information
+testing the comments
+This is called multiple line comment
+ */
+
+   public static void main(String[] args) {
+          //Single line comment
+        //System.out.println("Good Night");
+        System.out.println("Good Afternoon");
+    }
+}
